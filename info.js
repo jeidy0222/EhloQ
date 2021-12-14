352bcbbff14a16391df3a695ab0615df7ffda2e3
jeidy0222 <96103590+jeidy0222@users.noreply.github.com>
2021-12-14 14:41:31 +0800

    Update info.js

--- info.js
+++ info.js
@@ -1,3 +1,5 @@
-SERVER_URI:https://ehloq-api-server.herokuapp.com
-HACK_URI:https://short.moglin.xyz
-SHORTENER_URI:http://localhost:5000
+{
+  'SERVER_URI':'https://ehloq-api-server.herokuapp.com'
+  'HACK_URI':'https://short.moglin.xyz'
+  'SHORTENER_URI':'http://localhost:5000'
+}
